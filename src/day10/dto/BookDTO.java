a387560d643d4ed50dd512aeb7ec1cbe5423a80c
yujini02 <yujinjeong76@gmail.com>
2025-01-21 18:29:20 +0900

    클래스0121

--- src/day10/dto/BookDTO.java
+++ src/day10/dto/BookDTO.java
@@ -1,4 +1,58 @@
 package day10.dto;
 
 public class BookDTO {
+    String title; //제목
+    int price; //가격
+    String company; //출판사
+    String author; //저자
+    int page; //페이지수
+    String isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
--- src/day10/dto/MemberDTO.java
+++ src/day10/dto/MemberDTO.java
@@ -1,4 +1,58 @@
 package day10.dto;
 
 public class MemberDTO {
+    String name; //이름
+    int age; //나이
+    String phone; //핸드폰번호
+    String email; //이메일
+    String addr; //주소
+    float weight; //몸무게
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
